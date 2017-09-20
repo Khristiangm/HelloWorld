68437a34cbf083a14361ff159d25e123838d3c05
Khristian Guimaraes Marchioro (BFSI - Brazil) <KH389659@wipro.com>
2017-09-20 13:16:45 -0300

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,31 @@
+/**
+ * Created by KH389659 on 18/09/2017.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = (5 + 35) + (2 * 20);
+        int mysecondnumber = 10;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mysecondnumber + myThirdNumber;
+        int myLastNumber = 1000 - myTotal;
+
+        System.out.println("My First Number Is:");
+        System.out.println( myFirstNumber);
+
+        System.out.println("My Second Number Is:");
+        System.out.println(mysecondnumber);
+
+        System.out.println("My Third Number Is:");
+        System.out.println(myThirdNumber);
+
+        System.out.println("My TOTAL Number Is:");
+        System.out.println(myTotal);
+
+        System.out.println("My Last Number Is:");
+        System.out.println(myLastNumber);
+
+    }
+}
